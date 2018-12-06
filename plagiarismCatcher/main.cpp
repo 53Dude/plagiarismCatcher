bc1979afe5c7efd9cadcb02a433143390aea2911
Guy <53dude@gmail.com>
2018-12-05 22:37:23 -0600

    Initial Commit

---
+++ plagiarismCatcher/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  plagiarismCatcher
+//
+//  Created by Guy on 12/5/18.
+//  Copyright © 2018 Guy Co. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
