7f128d34865758fbfe5e9778d57b05e107bc10e1
Guy <53dude@gmail.com>
2018-12-06 23:21:47 -0600

    Finished and commented

--- plagiarismCatcher/main.cpp
+++ plagiarismCatcher/main.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Guy Co. All rights reserved.
 //
 
+////////////////////    INCLUDES    ////////////////////
 #include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
@@ -19,11 +20,13 @@
 #include <cstdlib>
 #include <stdlib.h>
 
+////////////////////    CONSTANTS    ////////////////////
 #define MAX_FILE_NUM 1355
 #define HASH_SIZE 1000000
 
 using namespace std;
 
+////////////////////    DECLARES NODE STRUCTURE FOR THE HASH TABLE    ////////////////////
 class hashNode {
 public:
     int fileI;
@@ -39,26 +42,27 @@ public:
     }
     
 };
-
 hashNode* table[HASH_SIZE];
 
+////////////////////    KEY -> INDEX IN HASH TABLE    ////////////////////
 int hashI(string key){
     
     int ans=0;
     for (int i = 0; i < key.size(); i++) {
-        ans += 31*ans + key.at(i);
-        ans%=HASH_SIZE;
+        ans += 31*ans + key.at(i); //// sums characters ascii values with the past values*31
+        ans%=HASH_SIZE; //// modulus by hash table size to keep the index in range
     }
     
     return ans;
 }
 
+////////////////////    FILE IDX -> HASH TABLE W/ CHECKING 4 COLLISIONS    ////////////////////
 void put(string key,int curFile){
     
-    int idx=hashI(key);
+    int idx=hashI(key); //// gets hash table index for key
     hashNode *temp=table[idx];
     
-    if(temp==NULL){
+    if(temp==NULL){ //// if no data at index the node is added
         temp = new hashNode(curFile);
         
         temp->next=table[idx];
@@ -66,7 +70,7 @@ void put(string key,int curFile){
         return;
     }
     
-    while(temp->next!=NULL){
+    while(temp->next!=NULL){ //// if there are already nodes the new node is only added if the current file isn't already indexed
         if(temp->fileI==curFile){
             return;
         }
@@ -80,6 +84,7 @@ void put(string key,int curFile){
     
 }
 
+////////////////////    GIVEN DIRECTORY PULL PROGRAM    ////////////////////
 int getdir (string dir, vector<string> &files)
 {
     DIR *dp;
@@ -96,24 +101,38 @@ int getdir (string dir, vector<string> &files)
     return 0;
 }
 
+////////////////////    FORMATS N LENGTH STRINGS FOR HASHING    ////////////////////
 string strFormat(string input){
     string rtn;
     for(int i = 0; i < input.length(); i++) {
-        if(((int)input[i]>64&&(int)input[i]<91)||((int)input[i]>96&&(int)input[i]<123))
+        if(((int)input[i]>64&&(int)input[i]<91)||((int)input[i]>96&&(int)input[i]<123)) //// only adds alphabetical characters to the string
             rtn += input[i];
     }
-    transform(rtn.begin(), rtn.end(), rtn.begin(), ::tolower);
+    transform(rtn.begin(), rtn.end(), rtn.begin(), ::tolower); //// makes entire string lowercase
 
     return rtn;
 }
 
+////////////////////    NODE DECL FOR ALREADY PRINTED PAIRS    ////////////////////
 class dnEntry{
+    // this is here because basically my conversion from the hash table to the 2D array
+    // didn't work (sorry :( ) so I made this to hold all the pairs completed elements
+    // to check with so I don't get a double print out. I understand that it makes my
+    // program a little bit slower and it's not the most "elegant" solution but tbh I'm
+    // tired and the most elegant solutions are for programs I'm being paid for, pls
+    // don't be cross with me. Also, while I'm being honest I used two global variables
+    // in this program even though I know I'm not supposed to and I would be glad to
+    // explain and defend my use of them, you can email me at gsexton@utexas.edu or
+    // call me at 8062528544 and we can "hash" it out (; Thank you for reading and I
+    // hope this is also ample commenting to not get points taken off like i have for
+    // literally every other program. Thanks and have a great break!
 public:
     int x;
     int y;
 };
 vector<dnEntry> dne;
 
+////////////////////    CHECKS X&Y TO SEE IF THEY HAVE BEEN DONE BEFORE, SEE ABOVE PARAGRAPH FOR LONG EXPLANATION    ////////////////////
 bool done(int x,int y){
     
     for(int i=0;i<dne.size();i++){
@@ -127,6 +146,7 @@ bool done(int x,int y){
     return false;
 }
 
+////////////////////    THE "MAIN" EVENT    ////////////////////
 int main(int argc, const char * argv[]) {
     // insert code here...
     
@@ -138,44 +158,48 @@ int main(int argc, const char * argv[]) {
     const string& cPath = argv[1];
     const int n = atoi(argv[2]);
     
-    cout << "Path: " << cPath << endl;
-    cout << "n: " << n << endl << endl;
+//cout << "Path: " << cPath << endl;
+//cout << "n: " << n << endl << endl;
     
     vector<string> files = vector<string>();
     getdir(cPath,files);
-    files.erase(files.begin(),files.begin()+2);
+    files.erase(files.begin(),files.begin()+2); //// deletes "." & ".."
     
     string wPath=cPath+"/";
     vector<string> cFile = vector<string>();
     string tempWord;
-    string openFrom;
     
-    for (unsigned int i = 0;i < files.size();i++) {
+    for (unsigned int i = 0;i < files.size();i++) { //// does this for every file
 //cout << endl << i << ": " << wPath+files[i] << endl;
         
         fstream wFile;
         wFile.open((wPath+files[i]).c_str());
-        while(wFile >> tempWord&&cFile.size()<6){
-                tempWord=strFormat(tempWord);
+        while(wFile >> tempWord&&cFile.size()<n){
+                tempWord=strFormat(tempWord); //// reads in words and removes punctuations and spaces then adds to working vector of words
                 cFile.push_back(tempWord);
 //cout<< tempWord;
         }
-        
-        tempWord=cFile.at(0)+cFile.at(1)+cFile.at(2)+cFile.at(3)+cFile.at(4)+cFile.at(5);
+        tempWord="";
+        for(int i=0;i<n;i++){
+            tempWord+=cFile.at(i); //// concatonates first 6 words from vector into one string and removes the first word from the vector
+        }
         cFile.erase(cFile.begin());
-        put(tempWord,i);
+        put(tempWord,i); //// adds string to hash table
         
-        while(wFile >> tempWord){
+        while(wFile >> tempWord){ //// continues this process until eof
             tempWord=strFormat(tempWord);
             cFile.push_back(tempWord);
-            tempWord=cFile.at(0)+cFile.at(1)+cFile.at(2)+cFile.at(3)+cFile.at(4)+cFile.at(5);
+            tempWord="";
+            for(int i=0;i<n;i++){
+                tempWord+=cFile.at(i);
+            }
             cFile.erase(cFile.begin());
             put(tempWord,i);
         }
-        cFile.clear();
+        cFile.clear(); //// clears out vector for next file to use
     }
     
-    int sim[MAX_FILE_NUM ][MAX_FILE_NUM ];
+    int sim[MAX_FILE_NUM ][MAX_FILE_NUM ]; //// 2D array of similarities between files (collisions in the hash table), is initialized to 0 right after
     for(int i=0;i<files.size();i++)
         for(int j=0;j<files.size();j++)
             sim[i][j]=0;
@@ -184,7 +208,7 @@ int main(int argc, const char * argv[]) {
     hashNode *temp2;
     hashNode *del;
     
-    for(int i=0;i<HASH_SIZE;i++){
+    for(int i=0;i<HASH_SIZE;i++){ //// scans the hash table and counts the amount of collosions in the table and adds to the corresponding cell in the sim array
         if(table[i]!=NULL&&table[i]->next!=NULL){
             temp=table[i];
             temp2=table[i];
@@ -197,7 +221,7 @@ int main(int argc, const char * argv[]) {
                 temp2=table[i];
             }
         }
-        while(table[i]!=NULL){
+        while(table[i]!=NULL){ //// deallocates the hash table
             del=table[i];
             table[i]=table[i]->next;
             delete(del);
@@ -208,7 +232,7 @@ int main(int argc, const char * argv[]) {
     int bigY=0;
     bool cont=true;
     
-    while(cont){
+    while(cont){ //// searches through for the biggest element and prints it out then empties the cell in sim and does it again until no remaining cells are bigger than 200
         cont=false;
         for(int i=0;i<files.size();i++){
             for(int j=0;j<files.size();j++){
@@ -224,6 +248,8 @@ int main(int argc, const char * argv[]) {
         }
         sim[bigX][bigY]=0;
     }
+    
+    //// all done! Thanks for reading through my code! Have a great break and thank y'all again for a great semester!!
     
 //std::cout << "Hello, World!\n";
     return 0;
